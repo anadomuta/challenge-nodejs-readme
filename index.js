54b4a8eaab9b587b5d32d14b04dced0e71ffabc7
anadomuta <ana.domuta@hotmail.de>
2024-02-16 17:50:30 +0000

    adds questions and prompts to user

--- index.js
+++ index.js
@@ -1,21 +1,108 @@
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+  {
+    type: "input",
+    name: "username",
+    message: "What is your GitHub username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+  },
+  {
+    type: "input",
+    name: "project",
+    message: "What is your project's name?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please write a short description og your project.",
+  },
+  {
+    type: "list",
+    name: "license",
+    message:
+      "What kind of license should your project have? Please pick one of the licenses from the list or select None if you would like no licence.",
+    choices: [
+      "Academic Free License v3.0",
+      "Apache license 2.0",
+      "Artistic-2.0",
+      "Boost Software License 1.0",
+      'BSD 2-clause "Simplified" license',
+      'BSD 3-clause "New" or "Revised" license',
+      "BSD 3-clause Clear license",
+      'BSD 4-clause "Original" or "Old" license',
+      "BSD Zero-Clause license",
+      "Creative Commons license family",
+      "Creative Commons Zero v1.0 Universal",
+      "Creative Commons Attribution 4.0",
+      "Creative Commons Attribution ShareAlike 4.0",
+      "Do What The F*ck You Want To Public License",
+      "Educational Community License v2.0",
+      "Eclipse Public License 1.0",
+      "Eclipse Public License 2.0",
+      "European Union Public License 1.1",
+      "GNU Affero General Public License v3.0",
+      "GNU General Public License family",
+      "GNU General Public License v2.0",
+      "GNU General Public License v3.0",
+      "GNU Lesser General Public License family",
+      "GNU Lesser General Public License v2.1",
+      "GNU Lesser General Public License v3.0",
+      "ISC",
+      "LaTeX Project Public License v1.3c",
+      "Microsoft Public License",
+      "MIT",
+      "Mozilla Public License 2.0",
+      "Open Software License 3.0",
+      "PostgreSQL License",
+      "SIL Open Font License 1.1",
+      "University of Illinois/NCSA Open Source License",
+      "The Unlicense",
+      "zLib License",
+      "None",
+    ],
+  },
+  {
+    type: "input",
+    name: "dependencies",
+    message: "What command should be run to install dependencies",
+    default: "npm i",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "What command should be run to run tests",
+    default: "npm test",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What does the user need to know about using the repo?",
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message: "What does the user need to know about contributing to the repo?",
+  },
 ];
 
+inquirer.prompt(questions).then((answers) => {
+  console.log(answers);
+});
+
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
-function init() {
-
-}
+function init() {}
 
 // function call to initialize program
 init();
