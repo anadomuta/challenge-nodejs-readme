9715c3eb7f3cdb9243ed705b2fee0eb5bd5145de
anadomuta <ana.domuta@hotmail.de>
2024-02-17 09:28:15 +0000

    adds basic structure for sections in README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,36 @@
-// function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+// Function to generate markdown for README
+const generateMarkdown = (answers) => `# ${answers.project}
 
-`;
-}
+## Description
+
+${answers.description}
+
+## Installation 
+
+To install the necessary dependencies, run the following command: 
+
+${answers.dependencies}
+
+## Usage
+
+${answers.usage}
+
+## Licence
+
+This project is licensed under the ${answers.license}.
+
+## Contributing
+
+${answers.contribution}
+
+## Tests
+
+To run tests, use the following command: 
+
+${answers.tests}
+
+## Questions 
+
+If you have any questions about this project, please reach out: https://github.com/${answers.username}, ${answers.email}`;
 
 module.exports = generateMarkdown;
